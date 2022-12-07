bcc4e35f087779b781cfe45e7b99c9191607eb44
Craig Chasseur <spoonboy42@gmail.com>
2022-12-06 21:36:05 -0800

    Day 7, Part 2

---
+++ puzzles/day07/part2.cc
@@ -0,0 +1,164 @@
+#include <cstddef>
+#include <iostream>
+#include <limits>
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "absl/log/check.h"
+#include "absl/strings/numbers.h"
+#include "absl/strings/str_split.h"
+#include "absl/strings/string_view.h"
+#include "util/io.h"
+
+namespace {
+
+class Directory;
+class File;
+
+class FsNode {
+ public:
+  virtual size_t ByteSize() const = 0;
+
+  absl::string_view Name() const { return name_; }
+
+  Directory* Parent() const { return parent_; }
+
+ protected:
+  explicit FsNode(std::string name, Directory* parent)
+      : name_(std::move(name)), parent_(parent) {}
+
+ private:
+  std::string name_;
+  Directory* parent_ = nullptr;
+};
+
+class Directory final : public FsNode {
+ public:
+  explicit Directory(std::string name, Directory* parent)
+      : FsNode(std::move(name), parent) {}
+
+  size_t ByteSize() const override;
+
+  void AddFile(std::unique_ptr<File> child) {
+    files_.emplace_back(std::move(child));
+  }
+
+  void AddDir(std::unique_ptr<Directory> child) {
+    dirs_.emplace_back(std::move(child));
+  }
+
+  Directory* SubDir(absl::string_view name) const {
+    for (const std::unique_ptr<Directory>& dir : dirs_) {
+      if (dir->Name() == name) return dir.get();
+    }
+    return nullptr;
+  }
+
+  size_t SmallestAtLeast(const size_t min_size, size_t smallest_so_far = std::numeric_limits<size_t>::max()) const {
+    if (ByteSize() >= min_size && ByteSize() < smallest_so_far) {
+      smallest_so_far = ByteSize();
+    }
+    for (const std::unique_ptr<Directory>& child : dirs_) {
+      smallest_so_far = child->SmallestAtLeast(min_size, smallest_so_far);
+    }
+    return smallest_so_far;
+  }
+
+  size_t TotalSizeAtMost100K() const {
+    size_t total = 0;
+    if (ByteSize() <= 100000) {
+      total += ByteSize();
+    }
+    for (const std::unique_ptr<Directory>& child : dirs_) {
+      total += child->TotalSizeAtMost100K();
+    }
+    return total;
+  }
+
+ private:
+  std::vector<std::unique_ptr<File>> files_;
+  std::vector<std::unique_ptr<Directory>> dirs_;
+  mutable std::optional<size_t> cached_size_;
+};
+
+class File final : public FsNode {
+ public:
+  explicit File(std::string name, size_t byte_size, Directory* parent)
+      : FsNode(std::move(name), parent), byte_size_(byte_size) {}
+
+  size_t ByteSize() const override { return byte_size_; }
+
+ private:
+  size_t byte_size_;
+};
+
+Directory BuildTree(const std::vector<std::string>& shell_session) {
+  Directory root("/", nullptr);
+  Directory* current = &root;
+  bool in_listing = false;
+  for (absl::string_view line : shell_session) {
+    if (line.front() == '$') {
+      in_listing = false;
+      if (line.substr(2, 2) == "cd") {
+        const absl::string_view target = line.substr(5);
+        if (target == "/") {
+          current = &root;
+          continue;
+        }
+        if (target == "..") {
+          current = current->Parent();
+          CHECK_NE(current, nullptr);
+          continue;
+        }
+        current = current->SubDir(target);
+        CHECK_NE(current, nullptr);
+        continue;
+      }
+      CHECK_EQ(line.substr(2), "ls");
+      in_listing = true;
+      continue;
+    }
+    CHECK(in_listing);
+    std::vector<absl::string_view> parts = absl::StrSplit(line, ' ');
+    CHECK_EQ(2, parts.size());
+    if (parts.front() == "dir") {
+      current->AddDir(
+          std::make_unique<Directory>(std::string(parts.back()), current));
+      continue;
+    }
+    size_t filesize = 0;
+    CHECK(absl::SimpleAtoi(parts.front(), &filesize));
+    current->AddFile(
+        std::make_unique<File>(std::string(parts.back()), filesize, current));
+    continue;
+  }
+  return root;
+}
+
+size_t Directory::ByteSize() const {
+  if (cached_size_.has_value()) return *cached_size_;
+  size_t total = 0;
+  for (const std::unique_ptr<File>& child : files_) {
+    total += child->ByteSize();
+  }
+  for (const std::unique_ptr<Directory>& child : dirs_) {
+    total += child->ByteSize();
+  }
+  cached_size_ = total;
+  return total;
+}
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  CHECK_EQ(2, argc);
+  std::vector<std::string> lines = aoc::ReadLinesFromFile(argv[1]);
+  Directory root = BuildTree(lines);
+  const size_t free_space = 70000000 - root.ByteSize();
+  const size_t needed_space = 30000000 - free_space;
+  std::cout << root.SmallestAtLeast(needed_space) << "\n";
+  return 0;
+}
